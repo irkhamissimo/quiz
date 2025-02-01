c82bafa9b020da2b2525e6227741b0cb7eca0ec0
IrkhamN <56974703+irkhamissimo@users.noreply.github.com>
2025-02-01 17:05:44 +0700

    add QuestionTimer

--- src/components/Quizz.jsx
+++ src/components/Quizz.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
 import quizCompleteImg from "../assets/quiz-complete.png";
+import QuestionTimer from "./QuestionTimer.jsx";
 
 const Quizz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -28,6 +29,10 @@ const Quizz = () => {
   return (
     <div id="quiz">
       <div id="question">
+        <QuestionTimer
+          timeout={10000}
+          onTimeout={() => handleSelectAnswer(null)}
+        />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer) => (
